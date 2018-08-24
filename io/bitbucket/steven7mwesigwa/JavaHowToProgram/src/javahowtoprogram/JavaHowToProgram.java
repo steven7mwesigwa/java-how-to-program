10582e3833158350ba15c38efde551275101fb61
steven7mwesigwa <ripper1355@gmail.com>
2018-08-24 20:57:08 +0300

    First commit. Chapter 1

---
+++ io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/javahowtoprogram/JavaHowToProgram.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javahowtoprogram;
+
+/**
+ *
+ * @author jaguar
+ */
+public class JavaHowToProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hellflkjflkgfo Wine");
+        System.out.println("Hello Wine");
+        System.out.println("Hello Wine");
+        sod();
+    }
+    public static void sod() {
+        System.out.println("Hello wine");
+        
+    }
+    
+}
