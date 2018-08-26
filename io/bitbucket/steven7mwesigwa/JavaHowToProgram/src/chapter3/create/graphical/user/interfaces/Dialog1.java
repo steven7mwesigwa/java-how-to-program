79849b53e60ce6ca2a16aa066d0e3a2e90772721
steven7mwesigwa <ripper1355@gmail.com>
2018-08-26 23:00:26 +0300

    Created 'Dialog' class to display a GUI dialog box

---
+++ io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter3/create/graphical/user/interfaces/Dialog1.java
@@ -0,0 +1,17 @@
+// Using JOptionPane to display multiple lines in a dialog box.
+package chapter3.create.graphical.user.interfaces;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jaguar
+ */
+public class Dialog1 {
+
+    public static void main(String[] args) {
+        // display a dialog with a message
+        JOptionPane.showMessageDialog(null, "Welcome to Java");
+    }
+
+}
