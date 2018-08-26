53d7ab0b0fac610559f3201b2713be5a20b3cb97
steven7mwesigwa <ripper1355@gmail.com>
2018-08-26 05:13:00 +0300

    Solved Chapter-2 > Page-109 > Number-2.18

--- io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter2/drawshapes/Main.java
+++ io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter2/drawshapes/Main.java
@@ -10,6 +10,8 @@ public class Main {
         describeQuestion();
         DrawShape box = new Box();
         DrawShape diamond = new Diamond();
+        System.out.println("\nSOLUTION");
+        System.out.println("\n=================");
         System.out.println("\nDRAW THE BOX");
         System.out.println("\n=================\n");
         box.drawShape();
@@ -20,12 +22,14 @@ public class Main {
     }
 
     /**
-     *
      */
     public static void describeQuestion() {
         System.out.println("\n\nChapter 2: Introduction to Java Applications; Input/Output and Operators");
         System.out.println("Page: 109");
-        System.out.println("2.18 (Displaying Shapes with Asterisks) Write an application that displays a box, an oval, an arrow and a diamond using asterisks ( * ), as follows:\n");
+        System.out.println("QUESTION\n");
+        System.out.println("=============");
+        System.out.println("2.18 (Displaying Shapes with Asterisks) Write an application that displays a box, an oval, an ar-\n"
+                + "row and a diamond using asterisks ( * ), as follows:\n");
         System.out.println("*********      ***        *        *");
         System.out.println("*       *    *     *     ***      * *");
         System.out.println("*       *   *       *   *****    *   *");
---
+++ io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter3/bankaccount/Account.java
@@ -0,0 +1,28 @@
+
+package chapter3.bankaccount;
+
+/**
+ *
+ * @author jaguar
+ */
+public class Account {
+
+    private String name; // instance variable
+    
+    /**
+     * method to set the name in the object
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name; // store the name
+    }
+
+    /**
+     * method to retrieve the name from the object
+     * @return
+     */
+    public String getName() {
+        return name; // return value of name to caller
+    }
+
+}
