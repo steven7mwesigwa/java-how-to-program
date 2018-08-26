144989a30585e7ad2b7a2d556bc7329246726f47
steven7mwesigwa <ripper1355@gmail.com>
2018-08-26 18:38:26 +0300

    Added Balance functionality, a Constructor, 'deposit()' and 'getBalance()' methods.

--- io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter3/bankaccount/Account.java
+++ io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter3/bankaccount/Account.java
@@ -1,4 +1,3 @@
-
 package chapter3.bankaccount;
 
 //    -----------------------------------
@@ -10,9 +9,6 @@ package chapter3.bankaccount;
 //    | + getName() : String            |
 //    |                                 |
 //    -----------------------------------
-
-
-
 /**
  *
  * @author jaguar
@@ -20,9 +16,35 @@ package chapter3.bankaccount;
 public class Account {
 
     private String name; // instance variable
+    private double balance; // instance variable
+
+    // Account constructor that receives two parameters
+    public Account(String name, double balance) {
+        this.name = name; // assign name to instance variable name
+        // validate that the balance is greater than 0.0; if it's not,
+        // instance variable balance keeps its default initial value of 0.0
+        if (balance > 0.0) // if the balance is valid
+        {
+            this.balance = balance; // assign it to instance variable balance
+        }
+    }
+
+    // method that deposits (adds) only a valid amount to the balance
+    public void deposit(double depositAmount) {
+        if (depositAmount > 0.0) // if the depositAmount is valid
+        {
+            balance = balance + depositAmount; // add it to the balance
+        }
+    }
     
+    // method returns the account balance
+    public double getBalance() {
+        return balance;
+    }
+
     /**
      * method to set the name in the object
+     *
      * @param name
      */
     public void setName(String name) {
@@ -31,6 +53,7 @@ public class Account {
 
     /**
      * method to retrieve the name from the object
+     *
      * @return
      */
     public String getName() {
