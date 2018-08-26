94ef9a4db765faa2c34e40a3356b739899823938
steven7mwesigwa <ripper1355@gmail.com>
2018-08-26 18:48:36 +0300

    Modified 'AccountTest' class to cater for balances - of the individual Bank Accounts.

--- io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter3/bankaccount/AccountTest.java
+++ io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter3/bankaccount/AccountTest.java
@@ -1,5 +1,4 @@
-
-package chapter3.bankaccount; 
+package chapter3.bankaccount;
 
 /**
  *
@@ -12,20 +11,35 @@ import java.util.Scanner;
 public class AccountTest {
 
     public static void main(String[] args) {
-        // create a Scanner object to obtain input from the command window
+        Account account1 = new Account("Jane Green", 50.00);
+        Account account2 = new Account("John Blue", -7.53);
+// display initial balance of each object
+        System.out.printf("%s balance: $ %.2f %n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $ %.2f %n%n",
+                account2.getName(), account2.getBalance());
+// create a Scanner to obtain input from the command window
         Scanner input = new Scanner(System.in);
-// create an Account object and assign it to myAccount
-        Account myAccount = new Account();
-// display initial value of name (null)
-        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
-// prompt for and read name
-        System.out.println("Please enter the name:");
-        String theName = input.nextLine(); // read a line of text
-        myAccount.setName(theName); // put theName in myAccount
-        System.out.println(); // outputs a blank line
-// display the name stored in object myAccount
-        System.out.printf("Name in object myAccount is:%n%s%n",
-                myAccount.getName());
+        System.out.print("Enter deposit amount for account1: "); // prompt
+        double depositAmount = input.nextDouble(); // obtain user input
+        System.out.printf("%nadding %.2f to account1 balance%n%n",
+                depositAmount);
+        account1.deposit(depositAmount); // add to account1’s balance
+// display balances
+        System.out.printf("%s balance: $ %.2f %n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $ %.2f %n%n",
+                account2.getName(), account2.getBalance());
+        System.out.print("Enter deposit amount for account2: "); // prompt
+        depositAmount = input.nextDouble(); // obtain user input
+        System.out.printf("%nadding %.2f to account2 balance%n%n",
+                depositAmount);
+        account2.deposit(depositAmount); // add to account2 balance
+// display balances
+        System.out.printf("%s balance: $ %.2f %n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $ %.2f %n%n",
+                account2.getName(), account2.getBalance());
     }
 
 }
