f0e6c57373ff9da4b4a97ab7bf71bc391f1c5d05
steven7mwesigwa <ripper1355@gmail.com>
2018-08-28 00:02:48 +0300

    Refactored / renamed class name

---
+++ io/bitbucket/steven7mwesigwa/JavaHowToProgram/src/chapter3/create/graphical/user/interfaces/messagedialog/MessageDialog.java
@@ -0,0 +1,18 @@
+// Using JOptionPane to display multiple lines in a dialog box.
+package chapter3.create.graphical.user.interfaces.messagedialog;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jaguar
+ */
+public class MessageDialog {
+
+    public static void main(String[] args) {
+        // display a dialog with a message
+        JOptionPane.showMessageDialog(null, "Welcome to Java");
+//        JOptionPane.showMessageDialog(null, "alert", "alert", JOptionPane.ERROR_MESSAGE);
+    }
+
+}
